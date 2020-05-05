c7105ff70b1201ddffed8327bc4cdf89513b4de8
vianney <vianney@dysbeat.fr>
2020-05-05 09:21:45 +0200

    Adapt to mobile screen.

--- src/components/ResultDay.svelte
+++ src/components/ResultDay.svelte
@@ -3,7 +3,7 @@
   export let day_number = 0;
 
   function MakeInfo(x) {
-    return x.date + (x.place ? " " + x.place.toLowerCase() : "");
+    return x.date + (x.place ? " " + x.place.toUpperCase() : "");
   }
 </script>
 
@@ -28,7 +28,7 @@
     align-items: center;
     justify-content: center;
     font-weight: 600;
-    font-size: 30px;
+    font-size: 24px;
   }
 
   .team.a {
@@ -56,6 +56,7 @@
   }
 
   .score.highlight {
+    /* font-size: 30px; */
     /* text-decoration: underline; */
   }
 
@@ -73,7 +74,7 @@
 
   .header {
     display: grid;
-    grid-template-columns: auto 300px 50px 10px 50px 300px 100px auto;
+    grid-template-columns: auto 250px 50px 10px 50px 250px 100px auto;
     grid-template-rows: repeat(1, 40px);
     grid-template-areas: ". info . . . . . .";
     grid-column-gap: 0px;
@@ -81,7 +82,7 @@
 
   .results {
     display: grid;
-    grid-template-columns: auto 300px 50px 10px 50px 300px 100px auto;
+    grid-template-columns: auto 250px 50px 10px 50px 250px 100px auto;
     grid-template-rows: repeat(3, 40px);
     grid-template-areas:
       ". team-a score-a sep score-b team-b location ."
@@ -89,23 +90,62 @@
       ". team-a score-a sep score-b team-b location .";
     grid-column-gap: 0px;
   }
+
+  @media screen and (max-width: 768px) {
+    .info {
+      justify-content: center;
+    }
+
+    .team {
+      justify-content: left;
+    }
+
+    .header {
+      display: grid;
+      grid-template-columns: auto 250px 50px auto;
+      grid-template-rows: repeat(1, 40px);
+      grid-template-areas: ". info . .";
+      grid-column-gap: 0px;
+    }
+
+    .results {
+      display: grid;
+      grid-template-columns: auto 250px 50px auto;
+      grid-template-rows: repeat(2, 40px);
+      grid-template-areas:
+        ". team-a score-a ."
+        ". team-b score-b .";
+      grid-row-gap: 0px;
+    }
+
+    .location {
+      display: none;
+    }
+    .sep {
+      border-bottom: 1px solid;
+      border-color: hsl(203, 15%, 20%);
+    }
+  }
 </style>
 
 <div class="header">
   <p class="info gray">Jour {day_number}</p>
 </div>
-{#each day as result}
+{#each day as result, idx}
+  {#if idx != 0}
+    <div class="sep" />
+  {/if}
   <div class="results">
     <p
       class="light-gray team a {result.ateam == 'Dunkerque' ? 'light-blue' : ''}">
       {result.ateam}
     </p>
     <p
-      class="gray score a {+result.ascore > +result.bscore ? 'highlight' : ''}">
+      class="gray score a {+result.ascore > +result.bscore ? '' : 'lighter-gray'}">
       {result.ascore}
     </p>
     <p
-      class="gray score b {+result.bscore > +result.ascore ? 'highlight' : ''}">
+      class="gray score b {+result.bscore > +result.ascore ? '' : 'lighter-gray'}">
       {result.bscore}
     </p>
     <p
